4d7b58252ee8624a40c70b179e7bed22c5d0a51e
chiscake <kirilloleshkevich7@gmail.com>
2026-05-22 18:52:14 +0300

    fix: enhance dotenv configuration for directory-specific environment loading

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,7 +1,16 @@
 import dotenv from 'dotenv'
 import { createClient } from '@supabase/supabase-js';
 
-dotenv.config({ path: '.env.local' })
+const currentDir = process.cwd().split('\\').pop();
+
+if (currentDir === 'your-signature-ai') {
+    dotenv.config({ path: 'site/.env.local' });
+} else if (currentDir === 'site') {
+    dotenv.config({ path: '.env.local' });
+} else {
+    console.error('Current directory is not supported: ' + currentDir);
+    process.exit(1);
+}
 
 export function createSupabaseClient() {
     return createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_SECRET || process.env.SUPABASE_SERVICE_ROLE_KEY);
